b12e3f959f462a678ac70520c3d5549046f4fb54
Martin Hardselius <martin@hardselius.dev>
2021-11-14 23:36:59 +0100

    fixup! Add nix stuff

--- flake.nix
+++ flake.nix
@@ -14,12 +14,22 @@
   outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [
+          (import rust-overlay)
+        ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
         avrlibc = pkgs.pkgsCross.avr.libcCross;
-
+        avr_incflags = [
+          "-isystem ${avrlibc}/avr/include"
+          "-B${avrlibc}/avr/lib/avr5"
+          "-L${avrlibc}/avr/lib/avr5"
+          "-B${avrlibc}/avr/lib/avr35"
+          "-L${avrlibc}/avr/lib/avr35"
+          "-B${avrlibc}/avr/lib/avr51"
+          "-L${avrlibc}/avr/lib/avr51"
+        ];
       in
       rec {
         # `nix develop`
@@ -33,8 +43,8 @@
               })
               rust-analyzer
 
-              pkgsCross.avr.buildPackages.binutils
-              pkgsCross.avr.buildPackages.gcc8
+              pkgsCross.avr.buildPackages.binutils # TODO: avr-ld cannot find -liconv
+              pkgsCross.avr.buildPackages.gcc11
               avrlibc
               avrdude
             ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
@@ -43,11 +53,14 @@
             ]);
 
             AVR_CPU_FREQUENCY_HZ = 16000000;
-            # AVR_CFLAGS = avr_incflags;
-            # AVR_ASFLAGS = avr_incflags;
+            AVR_CFLAGS = avr_incflags;
+            AVR_ASFLAGS = avr_incflags;
             RUST_BACKTRACE = 1;
 
             shellHook = ''
+              # Prevent the avr-gcc wrapper from picking up host GCC flags like
+              # -iframework, which is problematic on Darwin
+              unset NIX_CFLAGS_COMPILE_FOR_TARGET
               export PATH=$PATH:$HOME/.cargo/bin
             '';
           };
